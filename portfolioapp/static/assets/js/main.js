d6c6e1785c16666b3823c57e823062607c9dc99e
albertgl99 <albertgl99work@gmail.com>
2025-01-02 14:49:41 +0100

    js

--- portfolioapp/static/assets/js/main.js
+++ portfolioapp/static/assets/js/main.js
@@ -1,25 +1,31 @@
-window.addEventListener("scroll", function () {
-  const scrollTopBtn = document.getElementById("scrollTopBtn");
-  if (window.scrollY > 300) {
-    gsap.to(scrollTopBtn, { duration: 0.3, opacity: 1, display: "flex" });
-  } else {
-    gsap.to(scrollTopBtn, { duration: 0.3, opacity: 0, display: "none" });
+document.addEventListener("DOMContentLoaded", () => {
+  // Mostrar/Ocultar botón "Scroll to Top"
+  window.addEventListener("scroll", function () {
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+    gsap.set(scrollTopBtn, { display: window.scrollY > 300 ? "flex" : "none" });
+    gsap.to(scrollTopBtn, { duration: 0.3, opacity: window.scrollY > 300 ? 1 : 0 });
+  });
+
+  // Función para desplazarse al inicio
+  function scrollToTop() {
+    gsap.to(window, { duration: 1, scrollTo: { y: 0 }, ease: "power2.inOut" });
   }
+
+  // Función para desplazarse a secciones
+  function $scroll(id) {
+    const element = document.querySelector(id);
+    if (element) {
+      gsap.to(window, {
+        duration: 1,
+        scrollTo: { y: element, offsetY: 50 },
+        ease: "power2.inOut",
+      });
+    } else {
+      console.error(`No se encontró el elemento con el ID: ${id}`);
+    }
+  }
+
+  // Exponer las funciones al ámbito global (si es necesario)
+  window.scrollToTop = scrollToTop;
+  window.$scroll = $scroll;
 });
-
-function scrollToTop() {
-  gsap.to(window, { duration: 1, scrollTo: { y: 0 }, ease: "power2.inOut" });
-}
-
-function $scroll(id) {
-  const element = document.querySelector(id);
-  if (element) {
-    gsap.to(window, {
-      duration: 1,
-      scrollTo: { y: element, offsetY: 50 }, // offsetY para ajustar con un menú fijo si lo tienes
-      ease: "power2.inOut",
-    });
-  } else {
-    console.error(`No se encontró el elemento con el ID: ${id}`);
-  }
-}
