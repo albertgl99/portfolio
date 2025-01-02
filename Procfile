3f29120cd27dd38f31d1b43ff8babffb26b9be03
albertgl99 <albertgl99work@gmail.com>
2025-01-02 12:57:37 +0100

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn portfolioapp.wsgi --log-file -
+web: gunicorn portfolio.wsgi --log-file -
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Agrega esta línea
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
